077ef0d8f437d3f195721550db04ce2af8953603
Joshua Hall <alamoscout117@gmail.com>
2022-11-02 21:22:36 -0400

    starting the addition of purple notes

--- app.py
+++ app.py
@@ -16,6 +16,13 @@ GN_UPPER = np.array([72, 255, 255])
 # Red star notes would sometimes miss, therefore I lowered the value to increase match chance of red.
 R_LOWER = np.array([0, 100, 70])
 R_UPPER = np.array([10, 255, 255])
+
+####
+# TEST VARIABLES #
+####
+# T_R_LOWER = np.array([14, 0, 85])
+# T_R_UPPER = np.array([27, 30, 255])
+
 YE_LOWER = np.array([20, 100, 100])
 YE_UPPER = np.array([40, 255, 255])
 # Blue and orange are unique, the note color and the hold note line are not the exact same color so the line can be completely erased.
@@ -24,6 +31,9 @@ BL_UPPER = np.array([120, 255, 255])
 # Orange saturation would sometimes catch the thick white line. This shouldn't cause a note issue as their saturation is higher.
 OR_LOWER = np.array([19, 178, 100])
 OR_UPPER = np.array([21, 255, 255])
+
+PUR_LOWER = np.array([140, 100, 100])
+PUR_UPPER = np.array([145, 255, 255])
 
 class PlayRedux(threading.Thread):
     def __init__(self):
@@ -56,6 +66,9 @@ class PlayRedux(threading.Thread):
         self.or_lower = OR_LOWER
         self.or_upper = OR_UPPER
         
+        # self.t_lower = T_R_LOWER
+        # self.t_upper = T_R_UPPER
+        
     def start_playing(self):
         self.running = True
         
@@ -79,7 +92,6 @@ class PlayRedux(threading.Thread):
         ye_mask_bg = cv2.inRange(converted, self.ye_lower, self.ye_upper)
         bl_mask_bg = cv2.inRange(converted, self.bl_lower, self.bl_upper)
         or_mask_bg = cv2.inRange(converted, self.or_lower, self.or_upper)
-        
         # Green needs to be 2 higher due to star notes very rarely not hitting the box, thus missing the note.
         self.gn_bg = gn_mask_bg[54:72, 77:95]
         self.gn_bg_t = gn_mask_bg[52:56, 48:52]
@@ -93,7 +105,7 @@ class PlayRedux(threading.Thread):
         self.bl_bg = bl_mask_bg[56:67, 387:500]
         self.bl_bg_t = bl_mask_bg[52:56, 435:439]
         
-        self.or_bg = or_mask_bg[54:72, 530:558]
+        self.or_bg = or_mask_bg[54:72, 530:643]
         self.or_bg_t = or_mask_bg[52:56, 569:573]
         
     def set_area(self):
@@ -106,6 +118,11 @@ class PlayRedux(threading.Thread):
             self.ye_mask = cv2.inRange(converted, self.ye_lower, self.ye_upper)
             self.bl_mask = cv2.inRange(converted, self.bl_lower, self.bl_upper)
             self.or_mask = cv2.inRange(converted, self.or_lower, self.or_upper)
+            
+        
+            # self.test_mask = cv2.inRange(converted, self.t_lower, self.t_upper)
+            
+            
             # Green needs to be 2 higher due to star notes very rarely not hitting the box, thus missing the note.
             self.gn_chk = self.gn_mask[54:72, 77:95]
             self.gn_chk_t = self.gn_mask[52:56, 48:52]
@@ -120,8 +137,10 @@ class PlayRedux(threading.Thread):
             self.bl_chk = self.bl_mask[56:67, 387:500]
             self.bl_chk_t = self.bl_mask[52:56, 435:439]
             
-            self.or_chk = self.or_mask[54:72, 530:558]
+            self.or_chk = self.or_mask[54:72, 530:643]
             self.or_chk_t = self.or_mask[52:56, 569:573]
+            
+            # self.pur_chk = 
             
     def set_times(self):
         self.green_strum = current_time()
@@ -293,24 +312,25 @@ class PlayRedux(threading.Thread):
                             self.played = True
                             self.notes.append('f')
                     if (len(self.notes) > 0):
+                        # self.img_check = self.test_mask
                         self.save_image()
                         self.release_all()
                         self.strum()
-                    elif(np.sum(self.gn_df_t) > 0):
-                        self.g_low2.append(np.sum(self.gn_df))
-                        # self.img_check = self.gn_mask
-                        # self.save_image()
+                    # elif(np.sum(self.gn_df_t) > 0):
+                    #     self.g_low2.append(np.sum(self.gn_df))
+                    #     # self.img_check = self.gn_mask
+                    #     self.save_image()
                     # elif(np.sum(self.r_df_t) > 0):
-                    #     self.img_check = self.r_mask
+                    #     # self.img_check = self.r_mask
                     #     self.save_image()
                     # elif(np.sum(self.ye_df_t) > 0): 
-                    #     self.img_check = self.ye_mask
+                    #     # self.img_check = self.ye_mask
                     #     self.save_image()
                     # elif(np.sum(self.bl_df_t) > 0): 
-                    #     self.img_check = self.bl_mask
+                    #     # self.img_check = self.bl_mask
                     #     self.save_image()
                     # elif(np.sum(self.or_df_t) > 0):
-                    #     self.img_check = self.or_mask
+                    #     # self.img_check = self.or_mask
                     #     self.save_image()
                     # print(current_time() - start)
             time.sleep(0.01) 
@@ -329,14 +349,14 @@ def on_press(key):
             y=1
             play_thread.g_low.sort()
             play_thread.g_low2.sort()
-            print('Green low: ' + str(play_thread.g_low[0]))
-            print('Green low 2: ' + str(play_thread.g_low2[0]))
+            # print('Green low: ' + str(play_thread.g_low[0]))
+            # print('Green low 2: ' + str(play_thread.g_low2[0]))
             for x in play_thread.images:
                 cv2.rectangle(x['image'], (76, 53), (96,73), (255,0,0), 1)
                 cv2.rectangle(x['image'], (212, 55), (228,73), (255,0,0), 1)
                 cv2.rectangle(x['image'], (344, 53), (373,73), (255,0,0), 1)
                 cv2.rectangle(x['image'], (386, 55), (501,68), (255,0,0), 1)
-                cv2.rectangle(x['image'], (529, 55), (559,68), (255,0,0), 1)
+                cv2.rectangle(x['image'], (529, 55), (644,68), (255,0,0), 1)
                 
                 cv2.rectangle(x['image'], (47, 51), (53,57), (255,0,0), 1)
                 cv2.rectangle(x['image'], (178, 51), (184,57), (255,0,0), 1)
@@ -344,7 +364,7 @@ def on_press(key):
                 cv2.rectangle(x['image'], (434, 51), (440,57), (255,0,0), 1)
                 cv2.rectangle(x['image'], (568, 51), (574,57), (255,0,0), 1)
                 
-                cv2.imwrite('img_{}_g{}_r{}_y{}_b{}_o{}_gt{}_rt{}_yt{}_bt{}_ot{}_{}.png'.format(y, 
+                cv2.imwrite('imgs\img_{}_g{}_r{}_y{}_b{}_o{}_gt{}_rt{}_yt{}_bt{}_ot{}_{}.png'.format(y, 
                                                                                         x['green'], x['red'], x['yellow'], x['blue'], x['orange'],
                                                                                         x['green_t'], x['red_t'], x['yellow_t'], x['blue_t'], x['orange_t'],
                                                                                         x['played']), cv2.cvtColor(x['image'], cv2.COLOR_BGR2RGB))
