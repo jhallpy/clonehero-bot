b18a67f90694455126b2c2169e977a38d198ab5e
Joshua Hall <alamoscout117@gmail.com>
2022-11-06 00:40:05 -0400

    expanded areas of capture

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ stop_key = KeyCode(char='y')
 GN_LOWER = np.array([50, 133, 80])
 GN_UPPER = np.array([72, 255, 255])
 # Red star notes would sometimes miss, therefore I lowered the value to increase match chance of red.
-R_LOWER = np.array([0, 100, 70])
+R_LOWER = np.array([0, 100, 60])
 R_UPPER = np.array([10, 255, 255])
 
 YE_LOWER = np.array([20, 100, 100])
@@ -39,17 +39,6 @@ class PlayRedux(threading.Thread):
         self.program_running = True
         self.capture_area = capture_area
         self.images = []
-        
-        # self.g_low = []
-        # self.g_low2 = []
-        # self.r_low = []
-        # self.r_low2 = []
-        # self.y_low = []
-        # self.y_low2 = []
-        self.b_low = []
-        self.b_low2 = []
-        # self.o_low = []
-        # self.o_low2 = []
         
         self.gn_lower = GN_LOWER
         self.gn_upper = GN_UPPER
@@ -89,20 +78,20 @@ class PlayRedux(threading.Thread):
         or_mask_bg = cv2.inRange(converted, self.or_lower, self.or_upper)
         pur_mask_bg = cv2.inRange(converted, self.pur_lower, self.pur_upper)
         # Green needs to be 2 higher due to star notes very rarely not hitting the box, thus missing the note.
-        self.gn_bg = gn_mask_bg[54:72, 77:95]
-        self.gn_bg_t = gn_mask_bg[52:56, 48:52]
-
-        self.r_bg = r_mask_bg[56:72, 213:227]
-        self.r_bg_t = r_mask_bg[52:56, 179:183]
+        self.gn_bg = gn_mask_bg[54:90, 77:95]
+        self.gn_bg_t = gn_mask_bg[52:56, 10:70]
+        # Red's second tracker needs to be higher because of star notes.
+        self.r_bg = r_mask_bg[56:90, 213:227]
+        self.r_bg_t = r_mask_bg[49:56, 140:210]
         # Yellow needed to be 2 higher in order to have at least some number when checking if other notes were present.
         self.ye_bg = ye_mask_bg[54:72, 345:372]
-        self.ye_bg_t = ye_mask_bg[52:56, 310:314]
+        self.ye_bg_t = ye_mask_bg[52:56, 280:340]
         
         self.bl_bg = bl_mask_bg[56:67, 387:500]
-        self.bl_bg_t = bl_mask_bg[52:56, 435:439]
+        self.bl_bg_t = bl_mask_bg[52:56, 400:480]
         
         self.or_bg = or_mask_bg[54:72, 530:643]
-        self.or_bg_t = or_mask_bg[52:56, 569:573]
+        self.or_bg_t = or_mask_bg[52:56, 540:620]
         
         self.pur_bg = pur_mask_bg[54:72, 530:643]
         self.pur_bg_t = gn_mask_bg[54:72, 77:95]
@@ -122,21 +111,20 @@ class PlayRedux(threading.Thread):
             # self.test_mask = cv2.inRange(converted, self.t_lower, self.t_upper)
             
             # Green needs to be 2 higher due to star notes very rarely not hitting the box, thus missing the note.
-            self.gn_chk = self.gn_mask[54:72, 77:95]
-            self.gn_chk_t = self.gn_mask[52:56, 48:52]
-
-            self.r_chk = self.r_mask[56:72, 213:227]
-            self.r_chk_t = self.r_mask[52:56, 179:183]
-            
+            self.gn_chk = self.gn_mask[54:90, 77:95]
+            self.gn_chk_t = self.gn_mask[52:56, 10:70]
+            # Red's second tracker needs to be higher because of star notes.
+            self.r_chk = self.r_mask[56:90, 213:227]
+            self.r_chk_t = self.r_mask[49:56, 140:210]
             # Yellow needed to be 2 higher in order to have at least some number when checking if other notes were present.
             self.ye_chk = self.ye_mask[54:72, 345:372]
-            self.ye_chk_t = self.ye_mask[52:56, 310:314]
+            self.ye_chk_t = self.ye_mask[52:56, 280:340]
             
             self.bl_chk = self.bl_mask[56:67, 387:500]
-            self.bl_chk_t = self.bl_mask[52:56, 435:439]
+            self.bl_chk_t = self.bl_mask[52:56, 400:480]
             
             self.or_chk = self.or_mask[54:72, 530:643]
-            self.or_chk_t = self.or_mask[52:56, 569:573]
+            self.or_chk_t = self.or_mask[52:56, 540:620]
             
             self.pur_chk = self.pur_mask[54:72, 530:643]
             self.pur_chk_t = self.pur_mask[54:72, 77:95]
@@ -168,9 +156,8 @@ class PlayRedux(threading.Thread):
                             "green": np.sum(self.gn_df), "red": np.sum(self.r_df), "yellow": np.sum(self.ye_df), "blue": np.sum(self.bl_df), "orange": np.sum(self.or_df),
                             "green_t": np.sum(self.gn_df_t), "red_t": np.sum(self.r_df_t), "yellow_t": np.sum(self.ye_df_t), "blue_t": np.sum(self.bl_df_t), "orange_t": np.sum(self.or_df_t),
                             "played": self.played})
-     
+        
     def strum(self):
-        # key_press.release(Key.down)
         for x in self.notes:
             if (str(x) == 'p'):
                 continue
@@ -188,7 +175,7 @@ class PlayRedux(threading.Thread):
     def check_colors(self):
         if(np.sum(self.pur_df) > 100 and np.sum(self.pur_df_t) > 100 and current_time() - self.purple_strum > STRUM_TIME):
             self.purple_strum = current_time()
-            #Blue will strum the purple when it is a star note.
+            # NOTE: Blue will strum the purple when it is a star note.
             self.blue_strum = current_time()
             self.played = True
             self.notes.append('p')
@@ -222,67 +209,70 @@ class PlayRedux(threading.Thread):
                 self.capture()
                 self.notes = []
                 self.played = False
-                # start = current_time()
-                # dxcam will return None if the image it takes would be the exact same image as the previous image. Therefore, this check is necessary.
+                start = current_time()
+                # NOTE: dxcam will return None if the image it takes would be the exact same image as the previous image. Therefore, this check is necessary.
                 if (self.img_check is None):
                     continue
                 else:
                     self.set_area()
                     self.background_subtraction()
                     
-                    # Purple must come first. The purple bar when it is a 'star note' is within the range of blue. 
-                    # This keeps blue from firing on the inevitable next frame.
+                    # NOTE: Purple must come first. The purple bar when it is a 'star note' is within the range of blue. 
+                    # NOTE: The addition of the blue strum timeout fixes firing on the next frame. 
                     if(np.sum(self.pur_df) > 100 and current_time() - self.purple_strum > STRUM_TIME):
-                        print(str(current_time() - self.purple_strum) + " purple strum")    
+                        # print(str(current_time() - self.purple_strum) + " purple strum")    
                         self.check_colors()
                         
-                    if(np.sum(self.gn_df) > 1600 and current_time() - self.green_strum > STRUM_TIME):
-                        print(str(current_time() - self.green_strum) + " green strum")
+                    elif(np.sum(self.gn_df) > 1600 and current_time() - self.green_strum > STRUM_TIME):
+                        # print(str(current_time() - self.green_strum) + " green strum")
                         self.check_colors()
-                                
-                    elif(np.sum(self.r_df) > 3000 and current_time() - self.red_strum > STRUM_TIME):
-                        print(str(current_time() - self.red_strum) + " red strum")
+                        
+                    elif(np.sum(self.r_df) > 1500 and current_time() - self.red_strum > STRUM_TIME):
+                        # print(str(current_time() - self.red_strum) + " red strum")
                         self.check_colors()
                                   
                     elif(np.sum(self.ye_df) > 1500 and current_time() - self.yellow_strum > STRUM_TIME):
-                        print(str(current_time() - self.yellow_strum) + " yellow strum")
+                        # print(str(current_time() - self.yellow_strum) + " yellow strum")
                         self.check_colors()
                             
                     elif(np.sum(self.bl_df) > 1500 and current_time() - self.blue_strum > STRUM_TIME):
-                        print(str(current_time() - self.blue_strum) + " blue strum")
+                        # print(str(current_time() - self.blue_strum) + " blue strum")
                         self.check_colors()
                             
                     # Current highest orange that is not a note is 1530
                     elif(np.sum(self.or_df) > 1600 and current_time() - self.orange_strum > STRUM_TIME):
-                        print(str(current_time() - self.orange_strum) + " orange strum")
+                        # print(str(current_time() - self.orange_strum) + " orange strum")
                         self.check_colors()
                     
                     if (len(self.notes) > 0):
-                        self.save_image()
+                        # self.save_image()
                         self.release_all()
                         self.strum()
-                    elif(np.sum(self.pur_df) > 100):
-                        print(np.sum(self.pur_df))
-                        print(np.sum(self.pur_df_t))
+                        
+                    # NOTE: Leaving this commented for future debugging. I was too lazy to figure out best practices for debugging like this.
+                    # elif(np.sum(self.pur_df) > 100):
+                    #     print(np.sum(self.pur_df))
+                    #     print(np.sum(self.pur_df_t))
                     # elif(np.sum(self.gn_df_t) > 0):
-                    #     self.g_low2.append(np.sum(self.gn_df))
-                    #     # self.img_check = self.gn_mask
+                    #     self.img_check = self.gn_mask
                     #     self.save_image()
                     # elif(np.sum(self.r_df_t) > 0):
-                    #     # self.img_check = self.r_mask
+                    #     self.img_check = self.r_mask
                     #     self.save_image()
                     # elif(np.sum(self.ye_df_t) > 0): 
                     #     # self.img_check = self.ye_mask
                     #     self.save_image()
-                    elif(np.sum(self.bl_df_t) > 0 or np.sum(self.bl_df) > 0 ): 
-                        self.img_check = self.bl_mask
-                        self.b_low.append(np.sum(self.bl_df))
-                        self.b_low2.append(np.sum(self.bl_df_t))
-                        self.save_image()
+                    # elif(np.sum(self.bl_df_t) > 0 or np.sum(self.bl_df) > 0 ): 
+                    #     self.img_check = self.bl_mask
+                    #     self.save_image()
                     # elif(np.sum(self.or_df_t) > 0):
                     #     # self.img_check = self.or_mask
                     #     self.save_image()
-                    # print(current_time() - start)
+                # try:
+                #     print('FPS:', 1000 / (current_time() - start))
+                # except ZeroDivisionError:
+                #     print('FPS:', 1000)
+                        
             time.sleep(0.01) 
 
 play_thread = PlayRedux()
@@ -298,26 +288,27 @@ def on_press(key):
             play_thread.stop_playing()
             y=1
             
-            try:
-                play_thread.b_low.sort()
-                print('Blue low: ' + str(play_thread.b_low[0]))
-                play_thread.b_low2.sort()
-                print('Blue low 2: ' + str(play_thread.b_low2[0]))
-            except:
-                print('No blue.')
-                
+            # try:
+            #     play_thread.r_low.sort()
+            #     print('red low: ' + str(play_thread.r_low[0]))
+            #     play_thread.r_low2.sort(reverse=True)
+            #     print('red low 2: ' + str(play_thread.r_low2[0]))
+            # except:
+            #     print('No red.')
+
+            # NOTE: Only useful for saving images for debugging purposes. 
             for x in play_thread.images:
-                cv2.rectangle(x['image'], (76, 53), (96,73), (255,0,0), 1)
-                cv2.rectangle(x['image'], (212, 55), (228,73), (255,0,0), 1)
+                cv2.rectangle(x['image'], (76, 53), (96,91), (255,0,0), 1)
+                cv2.rectangle(x['image'], (212, 55), (228,91), (255,0,0), 1)
                 cv2.rectangle(x['image'], (344, 53), (373,73), (255,0,0), 1)
                 cv2.rectangle(x['image'], (386, 55), (501,68), (255,0,0), 1)
                 cv2.rectangle(x['image'], (529, 55), (644,68), (255,0,0), 1)
                 
-                cv2.rectangle(x['image'], (47, 51), (53,57), (255,0,0), 1)
-                cv2.rectangle(x['image'], (178, 51), (184,57), (255,0,0), 1)
-                cv2.rectangle(x['image'], (309, 51), (315,57), (255,0,0), 1)
-                cv2.rectangle(x['image'], (434, 51), (440,57), (255,0,0), 1)
-                cv2.rectangle(x['image'], (568, 51), (574,57), (255,0,0), 1)
+                cv2.rectangle(x['image'], (9, 51), (71,57), (255,0,0), 1)
+                cv2.rectangle(x['image'], (139, 48), (211,57), (255,0,0), 1)
+                cv2.rectangle(x['image'], (279, 51), (341,57), (255,0,0), 1)
+                cv2.rectangle(x['image'], (399, 51), (481,57), (255,0,0), 1)
+                cv2.rectangle(x['image'], (539, 51), (621,57), (255,0,0), 1)
                 
                 cv2.imwrite('imgs\img_{}_g{}_r{}_y{}_b{}_o{}_gt{}_rt{}_yt{}_bt{}_ot{}_{}.png'.format(y, 
                                                                                         x['green'], x['red'], x['yellow'], x['blue'], x['orange'],
@@ -325,7 +316,9 @@ def on_press(key):
                                                                                         x['played']), cv2.cvtColor(x['image'], cv2.COLOR_BGR2RGB))
                 y+=1
             play_thread.images = []
+            print('Bot Stopped.')
         else:
+            print('Bot Running...')
             play_thread.set_background()
             play_thread.release_all()
             play_thread.start_playing()
@@ -339,6 +332,6 @@ with Listener(on_press=on_press) as listener:
 
 # def main():
     
-    
+# NOTE: Should work but doesn't. Will look for fix when I finish the program.
 # if __name__ == "main":
 #     main()
